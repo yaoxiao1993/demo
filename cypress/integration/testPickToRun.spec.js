217162ba2e0258b41e5eaa72a46693e882610c1d
yaoxiaoyu <xiaoyu.yao@thoughtworks.com>
2020-07-15 17:29:12 +0800

    add testPickToRun

--- cypress/integration/testPickToRun.spec.js
+++ cypress/integration/testPickToRun.spec.js
@@ -1,21 +1,26 @@
 describe('动态挑选测试用例', () => {
-    it('Gets, types and asserts', () => {
-        cy.visit('https://example.cypress.io');
+    describe('运行一个测试块', ()=>{
+        it('Gets, types and asserts', () => {
+            cy.visit('https://example.cypress.io');
 
-        cy.contains('type').click()
+            cy.contains('type').click()
 
-        // Should be on a new URL which includes '/commands/actions'
-        cy.url().should('include', '/commands/actions')
+            // Should be on a new URL which includes '/commands/actions'
+            cy.url().should('include', '/commands/actions')
 
-        // Get an input, type into it and verify that the value has been updated
-        cy.get('.action-email')
-            .type('fake@email.com')
-            .should('have.value', 'fake@email.com')
+            // Get an input, type into it and verify that the value has been updated
+            cy.get('.action-email')
+                .type('fake@email.com')
+                .should('have.value', 'fake@email.com')
+        });
     });
 
-    it('测试动态挑选用例', ()=>{
-        cy.log('动态挑选用例');
+
+    it('使用自带方法挑选用例', ()=>{
+        cy.log('自带方法挑选用例');
     });
 
-    it('使用标签动态挑选测试用例')
+    it("[e2e, 'smoke'] 使用标签动态挑选测试用例", ()=>{
+        cy.log('标签挑选用例');
+    })
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,20 +15,26 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+//*************************************
+//指定新的环境配置文件
 const fs = require('fs-extra');
 const path = require('path');
 function getConfigByFile(file){
     const pathToConfigFile = path.resolve('..', 'demo/cypress/config', `cypress.${file}.json`);
     return fs.readJson(pathToConfigFile);
 }
+//*************************************
 
-const selectTestWithGrep = require('cypress-select-tests');
+//*************************************
+//通过标签挑选测试用例
+const selectTestWithGrep = require('cypress-select-tests/grep');
+//*************************************
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-    on('file:preprocessor', 'selectTestWithGrep(config)'); //通过标签挑选测试用例
+    on('file:preprocessor', selectTestWithGrep(config)); //通过标签挑选测试用例
 
-    // const file = config.env.configFile || 'dev'; //指定一个环境配置，如果没有指定，则使用cypress.dev.json
-    // return getConfigByFile(file);
+    const file = config.env.configFile || 'demo'; //指定一个环境配置，如果没有指定，则使用cypress.dev.json
+    return getConfigByFile(file);
 };
